e63a18589701e129e9124953b66d1e858a45fe99
John Karr <brainbuz@brainbuz.org>
2026-09-08 22:16:42 -0400

    update min perl to 5.036. replace Mojo::SQLite with DBI to stop bringing all of mojolicious as a dependency.

---
+++ lib/Finance/Tiller2QIF/DB.pm
@@ -0,0 +1,26 @@
+package Finance::Tiller2QIF::DB;
+# ABSTRACT: SQLite connection helper
+
+use v5.34;
+
+use DBI;
+use Exporter 'import';
+use feature qw/signatures/;
+
+our @EXPORT_OK = qw( connect_db );
+
+sub connect_db ($db_path) {
+  return DBI->connect(
+    "dbi:SQLite:dbname=$db_path",
+    '',
+    '',
+    {
+      RaiseError     => 1,
+      PrintError     => 0,
+      AutoCommit     => 1,
+      sqlite_unicode => 1,
+    }
+  );
+}
+
+1;
